91a56c3f62900f17a1649f4486c256028a67d24d
VSaverskyi <vovikck15@gmail.com>
2023-03-04 14:10:20 +0100

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector(".gallery"),
+  galleryItemEl: document.querySelector(".gallery__item"),
+  galleryLinkEl: document.querySelector(".gallery__link"),
+  galleryImageEl: document.querySelector(".gallery__image"),
+};
+
+const createGalleryItemsMarkup = function (items) {
+  return items
+    .map(({ preview, original, description } = {}) => {
+      return `
+          <div class="gallery__item">
+  <a class="gallery__link" href=${original}>
+  <img
+  class="gallery__image"
+  src=${preview}
+  data-source=${original}
+  alt=${description}
+  />
+  </a>
+  </div>
+  `;
+    })
+    .join("");
+};
+
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+const openModal = function (url) {
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+  instance.show();
+};
+const handleOpenModal = function (e) {
+  e.preventDefault();
+  openModal(e.target.dataset.source);
+};
+
+refs.galleryEl.innerHTML = galleryItemsMarkup;
+refs.galleryEl.addEventListener("click", handleOpenModal);
